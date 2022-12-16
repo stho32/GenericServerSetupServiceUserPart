d1f640e225b9607453721b0132b5002787142aa5
stho32 <stho32github@gmail.com>
2022-12-16 19:30:38 +0100

    Update run-lnac-server.sh

--- run-lnac-server.sh
+++ run-lnac-server.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-
+  
 # Set the URL of the zip file to download
-zip_url="http://example.com/path/to/zip/file.zip"
+zip_url="https://github.com/stho32/LocalNetAppChat/releases/download/v1.6/lnac-server-linux-x64.zip"
 
 # Set the name of the subdirectory to unzip the file into
-subdir_name="unzipped_files"
+subdir_name="lnac-server"
+
+rm -rf $subdir_name
 
 # Download the zip file
 wget "$zip_url"
 
 # Extract the zip file into the specified subdirectory
-unzip -d "$subdir_name" "file.zip"
+unzip -d "$subdir_name" "lnac-server-linux-x64.zip"
 
 # Remove the zip file
-rm "file.zip"
+rm "lnac-server-linux-x64.zip"
+
+cd $subdir_name
+
+chmod +x LocalNetAppChat.Server
+
+ip_address=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1)
+
+./LocalNetAppChat.Server --listenOn $ip_address --https --key somePassword
